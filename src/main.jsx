efc8a07c5bfa0d213d2f2361910bb17918e650f1
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-06-27 14:25:56 -0600

    feat: Add MainLayout component for improved page structure and layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import HomePage from './pages/HomePage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
+import MainLayout from './layouts/MainLayout.jsx';
 import ProductDetailPage from './pages/ProductDetailPage.jsx';
 import ProductsPage from './pages/ProductsPage.jsx';
 import ReactDOM from 'react-dom/client';
@@ -12,19 +13,25 @@ import { Toaster } from 'sonner';
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <HomePage />,
+		element: <MainLayout />,
+		children: [
+			{
+				path: '/',
+				element: <HomePage />,
+			},
+			{
+				path: '/products',
+				element: <ProductsPage />,
+			},
+			{
+				path: '/products/:id',
+				element: <ProductDetailPage />,
+			},
+		],
 	},
 	{
 		path: '/login',
 		element: <LoginPage />,
-	},
-	{
-		path: '/products',
-		element: <ProductsPage />,
-	},
-	{
-		path: '/products/:id',
-		element: <ProductDetailPage />,
 	},
 ]);
 
